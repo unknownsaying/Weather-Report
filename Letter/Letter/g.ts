ab3ab964bfb16d8e851a3801b982adb5c08bcd0a
unknownsaying <15368104568@189.cn>
2025-12-09 23:56:20 +0800

    Add files via upload

---
+++ Letter/Letter/g.ts
@@ -0,0 +1,5 @@
+'declare module' 
+export default function G():{}
+{return 'goto'; "=>'got'; => 'go'; => G"}
+export async function GENERICS()
+{"g->go->got->goto"}
---
+++ Letter/Letter/rain.ts
@@ -0,0 +1,20 @@
+case10:
+"interface == namespace"
+case11:
+"const == let"
+case12:
+"continue == break"
+
+case13:
+"type && typeof"
+case14:
+"string && number"
+case15:
+"never && class"
+
+case16:
+"public || private"
+case17:
+"var || static"
+case18:
+"any || never"
